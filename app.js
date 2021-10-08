4a77437bc82d752fa1f758ecb7f77c83c0522b1b
Anderson Ramos <andersonramos07@live.com>
2021-10-08 18:51:25 -0300

    Add new files

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('', (req, res) => {
     res.send('Hello World - AROS07');
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile('./about.html');
+})
+
 app.listen(PORT, () => {
     console.log('Este app está escutando na port ${PORT}');
 });
