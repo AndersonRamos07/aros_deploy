e25ec005ed50233536ed81b6f5e0f49149e17fa2
Anderson Ramos <andersonramos07@live.com>
2021-10-16 03:01:56 -0300

    letsgo

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const app = require('express')();   //usando o EXPRESS
-const bP = require('body-parser');  //usando o Body-Parser
+const bParser = require('body-parser');  //usando o Body-Parser
+
 const PORT = process.env.PORT || 8000;
+
+//  ------- //
+
+app.use(bParser.urlencoded({extended: false}));
+app.use(bParser.json());
 
 // HOME - INICIO
 app.get('', (req, res) => {
-    res.send('aros-deploy');
+    res.send('aros-deploy - redondo');
 });
 
 // ABOUT - SOBRE
 app.get('/about', (req, res) => {
     res.sendFile(__dirname + '/html/about.html');
-})
+});
+
+//  CAD - CADASTRO
+app.post('/cad', (req, res) => {
+    res.send('De boas ' +req.body.nome+'!');
+});
 
 app.listen(PORT, () => {
     console.log('Este app está escutando na port ${PORT}');
