56742a616cc436cba9f51a970c21864e85de6ceb
AndersonRamos07 <52046214+AndersonRamos07@users.noreply.github.com>
2022-05-28 16:30:26 -0400

    Create active.js
    Create active.js with the Trebit App

---
+++ Trebit/active.js
@@ -0,0 +1,18 @@
+const digaOi = () =>{
+   alert("Hi First!") 
+}
+
+const dimensoes = () =>{
+    console.log(altura + " " + largura)
+}
+
+const fecharModal = () =>{
+    document.querySelector("#modal").close();
+}
+const abrirModal = () =>{
+    document.querySelector("#modal").showModal();
+}
+
+
+var altura = window.screen.height;
+var largura = window.screen.width;
